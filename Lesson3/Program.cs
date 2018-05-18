7689317383a0f55e023bce66eb411cf0b615c508
Michael L. Stephens <mlstephens@.zrtlab.com>
2018-05-18 15:36:03 -0700

    Added class assembly to project. Modified console to display json value items.

--- Lesson3/Program.cs
+++ Lesson3/Program.cs
@@ -1,13 +1,15 @@
-﻿using System;
+﻿using Project_Json;
+using System;
 using System.Linq;
 
 namespace Lesson3
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] clArguments)
         {
-            clArguments.ToList().ForEach(cl => Console.WriteLine("{0}", cl));
+            new ArgumentJson(clArguments).ValueItems.OrderBy(vi => vi).ToList().ForEach(vi => Console.WriteLine("{0}", vi));
         }
     }
 }
