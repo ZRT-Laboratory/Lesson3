107284a8e33ea7d7f097e8b8af925b0ea5611720
Michael L. Stephens <mlstephens@.zrtlab.com>
2018-05-18 17:22:03 -0700

    Added unittesting to solution.

---
+++ Project_UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Project_UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Project_UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("00c19801-112d-403c-a502-03db46d21ae9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Project_UnitTest/UnitTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Project_UnitTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+        string _testFilePath = string.Empty;
+        const string _validJSONFile = "JSON_ValidFormat.json";
+        const string _invalidJSONFile = "JSON_InvalidFormat.json";
+        const string _validXMLFile = "XML_ValidFormat.xml";
+        const string _invalidXMLFile = "XML_InvalidFormat.xml";
+
+        public UnitTest()
+        {
+            _testFilePath = Directory.GetCurrentDirectory() + @"\TestFiles\";
+        }
+
+        [TestMethod]
+        public void AllTestFilesExist()
+        {
+            Assert.IsTrue(File.Exists(_testFilePath + _validJSONFile));
+            Assert.IsTrue(File.Exists(_testFilePath + _invalidJSONFile));
+            Assert.IsTrue(File.Exists(_testFilePath + _validXMLFile));
+            Assert.IsTrue(File.Exists(_testFilePath + _invalidXMLFile));
+        }
+
+        [TestMethod]
+        public void ValidArguments()
+        {
+            //arrange
+            string[] argArray = new string[] { "-json", _testFilePath, "-xml", _testFilePath };
+
+            //act
+            bool valid = argArray.Any(a => String.Compare(a, "-json", true) == 0) || argArray.Any(a => String.Compare(a, "-xml", true) == 0);
+
+            //assert
+            Assert.IsTrue(valid);
+        }
+
+        [TestMethod]
+        public void InValidArguments()
+        {
+            //arrange
+            string[] argArray = new string[] { "json", _testFilePath, "xml", _testFilePath };
+
+            //act
+            bool valid = argArray.Any(a => String.Compare(a, "-json", true) == 0) || argArray.Any(a => String.Compare(a, "-xml", true) == 0);
+
+            //assert
+            Assert.IsFalse(valid);
+        }
+    }
+}
