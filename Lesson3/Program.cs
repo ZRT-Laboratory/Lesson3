e31dab6e860ecb9c5a94b5a33fd6ef1e0cd24f02
Michael L. Stephens <mlstephens@.zrtlab.com>
2018-06-11 12:14:28 -0700

    Added new GetSortedData method.

--- Lesson3/Program.cs
+++ Lesson3/Program.cs
@@ -8,6 +8,24 @@ namespace Project.ConsoleApp
 {
     public class Program
     {
+        public static List<string> GetSortedData(string[] fileData)
+        {
+            List<string> sortedData = new List<string>();
+
+            //create a list then order it so nulls are last in the list
+            if (fileData != null)
+            {
+                sortedData = fileData
+                    .OrderBy(ifh => ifh)
+                    .ToList()
+                    .OrderBy(ifh => ifh == null)
+                    .ToList();
+            }
+
+            return sortedData;
+        }
+
+        [STAThread]
         public static void Main(string[] clArguments)
         {
             if (clArguments.Any(a => String.Compare(a, "-json", true) == 0) || clArguments.Any(a => String.Compare(a, "-xml", true) == 0))
@@ -16,12 +34,9 @@ namespace Project.ConsoleApp
                 IFileHandling ifhJson = GetJsonParser();
                 IFileHandling ifhXml = GetXmlParser();
 
-                //create a list then order it so nulls are last in the list
-                List<string> parsedData = ifhJson?.GetParsedData(GetFileData(GetFilePath("-json")))
-                    .Concat(ifhXml?.GetParsedData(GetFileData(GetFilePath("-xml"))))
-                    .OrderBy(ifh => ifh)
-                    .ThenBy(ifh => ifh == null)
-                    .ToList();
+                List<string> parsedData = GetSortedData(ifhJson?.GetParsedData(GetFileData(GetFilePath("-json")))
+                                                .Concat(ifhXml?.GetParsedData(GetFileData(GetFilePath("-xml")))).ToArray())
+                                            .ToList();
 
                 parsedData?.ForEach(pd => Console.WriteLine("{0}", pd));
             }
